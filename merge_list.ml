5b355def087b9bfac39b7cd909aeac7f6749e201
won <jhwgnu@hanmail.net>
2017-09-14 21:20:03 +0900

    testcase

--- merge_list.ml
+++ merge_list.ml
@@ -16,3 +16,23 @@ let l2 = [8;7;1]
 let result = merge(l1, l2)
 let () = List.iter(printf "%d ") result
 *)
+
+let _ =
+    let rec string_of_list = function
+        [] -> ""
+        | e::l -> string_of_int e ^ " " ^ string_of_list l
+    in
+    let assert_equal (expected: int list) (actual: int list) =
+        if expected = actual then print_endline "true"
+        else
+            let expected_string = string_of_list expected in
+            let actual_string = string_of_list actual in
+            Printf.printf "Expected %s but actual %s\n" expected_string actual_string
+    in
+    let test_merge (xs: int list) (ys: int list) (expected: int list) =
+        merge (xs, ys) |> assert_equal expected
+    in
+    test_merge [3;2;1] [4;3;2] [4;3;3;2;2;1];
+    test_merge [2;1] [10;9] [10;9;2;1];
+    test_merge [10;9] [4;3;2] [10;9;4;3;2];
+    test_merge [5;3;1] [6;4;2] [6;5;4;3;2;1]
--- sigma.ml
+++ sigma.ml
@@ -7,3 +7,17 @@ let foo a = a*2
 let result = sigma(3,5,foo)
 let _ = print_endline(string_of_int result)
 *)
+
+let _ =
+    let assert_equal (expected: int) (actual: int) =
+        if expected = actual then print_endline "true"
+        else Printf.printf "Expected %d but actual %d\n" expected actual
+    in
+    let test_sigma (a: int) (b: int) (f: int->int) (expected: int) =
+        sigma (a,b,f) |> assert_equal expected
+    in
+    test_sigma 1 2 (fun x -> x * x) 5;
+    test_sigma 1 100 (fun x -> x) 5050;
+    test_sigma 1 1 (fun x -> x) 1;
+    test_sigma 1 0 (fun x -> x) 0;
+    test_sigma 101 200 (fun x -> -x) (-15050);
